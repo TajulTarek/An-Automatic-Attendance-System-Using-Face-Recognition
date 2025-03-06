5deec7c0f506976175b09edf1c60e509cb5740a0
TajulTarek <tarekahmad484@gmail.com>
2025-03-06 22:44:39 +0600

    Re uploaded

---
+++ Server/config.py
@@ -0,0 +1,6 @@
+FACE_DESCRIPTOR_PATH = "./Model/face_descriptors_final.npy"
+MP_FILE_PATH = "./Model/mp_final.pkl"
+SHAPE_PREDICTOR_PATH = "./Weights/shape_predictor_68_face_landmarks.dat"
+FACE_RECOGNITION_MODEL_PATH = "./Weights/dlib_face_recognition_resnet_model_v1.dat"
+G1_CAMERA_URL="rtsp://admin:sarwarR45@10.100.32.123:554/Streaming/Channels/101"
+G2_CAMERA_URL="10.101.1.150:8080"
