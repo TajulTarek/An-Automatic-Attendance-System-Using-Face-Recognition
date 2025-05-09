1ace213ebf95623a8d1d6ef1ef40e297f0a05a9c
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-05-09 14:48:02 +0600

    Create StudentPhoto.js

---
+++ Backend/models/StudentPhoto.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const studentPhotoSchema = new mongoose.Schema({
+    registrationNumber: { 
+        type: String, 
+        required: true 
+    },
+    photos: [{ 
+        type: String, 
+        required: true 
+    }],
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    }
+});
+
+module.exports = mongoose.model('StudentPhoto', studentPhotoSchema); 
