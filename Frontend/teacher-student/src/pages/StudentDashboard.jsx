7ade2dcb33c0ce0e0003c552ca723b125aab57bb
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-05-09 14:37:55 +0600

    Update StudentDashboard.jsx

--- Frontend/teacher-student/src/pages/StudentDashboard.jsx
+++ Frontend/teacher-student/src/pages/StudentDashboard.jsx
@@ -133,13 +133,13 @@ const StudentDashboard = () => {
               </div>
             </div>
             <button
-              onClick={() => setIsModalOpen(true)}
+              onClick={() => navigate('/camera')}
               className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg transition duration-300 shadow-md"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
               </svg>
-              <span>Update Photo</span>
+              <span>Upload Photo</span>
             </button>
           </div>
         </div>
